147b7b869447aa31d294af5ea380a87132275c6f
michaelrex2012 <michaelgiu2012@outlook.com>
2024-06-10 17:01:03 -0400

    Extended setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,11 @@
 from setuptools import setup, find_packages
 
-VERSION = '1.0.2'
+VERSION = '1.0.3'
 DESCRIPTION = 'Simple functions for working with time'
-LONG_DESCRIPTION = ("# time-functions  "
-                    "Functions for working with time. There aren't any dependencies yet.  "
-                    "***  "
-                    "## Install and Update  "
-                    "Install this package with:  "
-                    "> python -m pip install time-functions  ")
+LONG_DESCRIPTION = "Functions for working with time. There aren't any dependencies yet."
 
 setup(
     name="time-functions",
-    long_description_content_type='text/markdown',
     version=VERSION,
     author="@michaelrex2012",
     author_email="<michaelgiu2012@outlook.com>",
